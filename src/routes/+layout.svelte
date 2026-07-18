f2d46e78f8ced0c0b96ac139c2b2d25d51ea11dd
dfdc1123 <dfdc1123@gmail.com>
2026-07-18 20:36:59 +0800

    fix(ui): disable application context menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import "../app.css";
   let { children } = $props();
+
+  function preventContextMenu(event: MouseEvent) {
+    event.preventDefault();
+  }
 </script>
+
+<svelte:window oncontextmenu={preventContextMenu} />
 
 {@render children()}
